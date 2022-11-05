ceab4abfb5a0c0ad72e391f848623b0170ce9f16
AjelFide <nervmyser@gmail.com>
2022-11-04 23:13:23 -0300

    ejercicio dia dos ok

--- codigo.js
+++ codigo.js
@@ -1,8 +1,23 @@
-let numeroUno
-let numeroDos
-const titulo
-const resultado
+let numeroUno;
+let numeroDos;
+
+const resultado = document.querySelector(".resultado");
 var booleanaUno
 var booleanaDos 
-let alumno = "Angel Fidel Bruguera"
-let comision = 22563
+let alumno;
+alumno = "Angel Fidel Bruguera"
+let comision;
+comision = 22563
+
+numeroUno = 35
+numeroDos = 30
+
+numeroUno *=2
+numeroUno +=5
+numeroUno *=50
+numeroUno +=60
+numeroUno -=365
+numeroUno +=115
+
+const edades = numeroUno;
+resultado.textContent = edades;
